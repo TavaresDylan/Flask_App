f08f5887647e0a69839f30b8262ccb1f293a3472
TavaresDylan <tavares-dylan@bbox.fr>
2021-03-16 13:38:39 +0100

    Routing some URL

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
+# Importation du framework flask
 from flask import Flask
-
+# Déclaration de l'application
 app = Flask(__name__)
-
+# Import du module de gestion des routes de flask
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,45 @@
 from app import app
-
+from flask import jsonify  # Import librairie permettant de rendre des contenues au format JSON
+from flask import render_template # Librairie permettant de rendre une vue html 
+from flask import request # Librairie permettant de faire passer des paramètres dans l'url avec la méthode GET
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("home.html")
+
+@app.route("/predict")
+def predict():
+    return "Prediction Page"
+
+@app.route("/hello")
+def hello():
+    jsontest = {
+        'success': True,
+        'data': "hello world"
+    }
+    return jsonify(jsontest)
+
+
+@app.route("/double",methods=['GET'])
+def double():
+    if not request.args.get('number'):
+        return "Empty parameters in URL, you must specify \"?number=x\" to multiply number x by 2"
+    else:
+        data_x2 = int(request.args.get('number')) * 2
+        double = {
+            "success": True,
+            "data": data_x2
+        }
+        return jsonify(double)
+
+@app.route("/triple",methods=['GET'])
+def triple():
+    if not request.args.get('number'):
+        return "Empty parameters in URL, you must specify \"?number=x\" to multiply number x by 3"
+    else:
+        data_x3 = int(request.args.get('number')) * 3
+        triple = {
+            "success": True,
+            "data": data_x3
+        }
+        return jsonify(triple)
