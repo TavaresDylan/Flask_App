4af55dedc951cb7813f36eb1f3f42e88f4ec4f0d
TavaresDylan <tavares-dylan@bbox.fr>
2021-03-16 14:56:08 +0100

    URL params prediction complete

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app import app
 ## Import des données d'exemple depuis les datasets de sklearn
-from sklearn.datasets import load_boston
-# Chargement des données splitter en data et target
-X, y = load_boston(return_X_y=True)
+#from sklearn.datasets import load_boston
+## Chargement des données splitter en data et target
+#X, y = load_boston(return_X_y=True)
--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,36 @@ from flask import request # Librairie permettant de faire passer des paramètres
 @app.route('/')
 @app.route('/index')
 def index():
+    # Rend une vue HTML dans le dossier templates
     return render_template("home.html")
 
-@app.route("/predict")
+@app.route("/predict",methods=['GET'])
 def predict():
-    return "Prediction Page"
+    if not request.args.get('sepal_length'):
+        return "Empty parameters in URL, you must specify \"?sepal_length=XXX&sepal_width=XXX&petal_length=XXX&petal_width=XXX\" to predict petal length"
+    else:
+        import numpy as np
+        import sklearn.datasets
+        ## Import des données d'exemple depuis les datasets de sklearn
+        from sklearn.datasets import load_iris
+        ## Chargement des données splitter en data et target
+        X, y = load_iris(return_X_y=True,as_frame=True)
+
+        from sklearn.linear_model import LinearRegression
+        from sklearn.model_selection import train_test_split
+
+        # Déclaration du modèle
+        lr = LinearRegression()
+        # Entrainement du modèle
+        lr.fit(X, y)
+        data_to_pred = [float(request.args.get("sepal_length")),float(request.args.get("sepal_width")),float(request.args.get("petal_width")),float(request.args.get("petal_length"))]
+        predict = lr.predict([data_to_pred])
+        data = load_iris(as_frame=True)
+        pred_json = {
+            "success": True,
+            "data": data.target_names[int(predict)]
+        }
+        return jsonify(pred_json)
 
 @app.route("/hello")
 def hello():
