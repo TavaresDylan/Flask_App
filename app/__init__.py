feecaf4e46750cb25109abcf4aef41570b0aa527
TavaresDylan <tavares-dylan@bbox.fr>
2021-03-23 14:42:02 +0100

    Flask_bootstrap4 integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 # Importation du framework flask
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 # Déclaration de l'application
 app = Flask(__name__)
 app.config.from_object(Config)
+Bootstrap(app)
 # Import du module de gestion des routes de flask
 from app import routes
--- app/form.py
+++ app/form.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class DoubleForm(FlaskForm):
     input_nb = StringField('Input number')
     submit = SubmitField('Calculate')
